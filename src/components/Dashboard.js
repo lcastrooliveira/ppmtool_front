0042d6b04cec31a14b2b6ad01f9059f3dfd57224
Lucas Oliveira <lcastro.oliveira@gmail.com>
2020-01-04 18:34:38 -0300

    Bringing bootstrap 4+

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,7 +5,7 @@ class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to our component</h1>
+        <h1 className="alert alert-warning">Welcome to our component</h1>
         <ProjectItem />
         <ProjectItem />
         <ProjectItem />
